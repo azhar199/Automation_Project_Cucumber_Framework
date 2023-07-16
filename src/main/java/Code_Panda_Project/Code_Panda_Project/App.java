812eea225109ad08d2453dbcec9aa7e7046d0434
unknown <Aimam9329@gmail.com>
2023-07-16 20:29:29 +0530

    some project change

--- src/main/java/Code_Panda_Project/Code_Panda_Project/App.java
+++ src/main/java/Code_Panda_Project/Code_Panda_Project/App.java
@@ -2,6 +2,7 @@ package Code_Panda_Project.Code_Panda_Project;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello testing application!!!");
+    System.out.println("Hello Azhar!!!");
+    System.out.println("moksancjosdadjncono");
   }
 }
